46253c8b8ea808c167839a6e9984a942b8f102b6
Marshall Porter <meoporter@gmail.com>
2026-04-01 10:41:15 -0700

    fmt and pipe

--- src/srv.ts
+++ src/srv.ts
@@ -1,5 +1,5 @@
-import { type Cmd, CmdBase } from "./cmd.ts";
-import { Fmt } from "./serde.ts";
+import { type Cmd, CmdBase } from './cmd.ts'
+import { Fmt } from './serde.ts'
 
 /**
  * This module contains components for building server implementations
@@ -16,24 +16,22 @@ export class SrvBase extends CmdBase implements Cmd {
    * @param scopes - Scope path for this command
    */
   constructor(scopes: Array<string>) {
-    super(scopes);
+    super(scopes)
     this.options = [
       {
-        keys: ["-f", "--format"],
-        description: `shell print format [${
-          Object.keys(Fmt).map((k, i) => !i ? k : `[${k}]`).join(", ")
-        }]`,
+        keys: ['-f', '--format'],
+        description: `shell print format [${Object.keys(Fmt).map((k, i) => !i ? k : `[${k}]`).join(', ')}]`,
       },
-    ];
+    ]
     this.switches = [
-      { keys: ["-d", "--debug"], description: "shell print debug" },
-      { keys: ["-g", "--grayscale"], description: "shell print skip color" },
-      { keys: ["-h", "--help"], description: "shell print help" },
-      { keys: ["-l", "--log"], description: "server print log" },
-      { keys: ["-n", "--noop"], description: "shell run skip" },
-      { keys: ["-s", "--succinct"], description: "shell print skip" },
-      { keys: ["-t", "--trace"], description: "shell print trace" },
-      { keys: ["-y", "--yes"], description: "shell run skip prompt" },
-    ];
+      { keys: ['-d', '--debug'], description: 'shell print debug' },
+      { keys: ['-g', '--grayscale'], description: 'shell print skip color' },
+      { keys: ['-h', '--help'], description: 'shell print help' },
+      { keys: ['-l', '--log'], description: 'server print log' },
+      { keys: ['-n', '--noop'], description: 'shell run skip' },
+      { keys: ['-s', '--succinct'], description: 'shell print skip' },
+      { keys: ['-t', '--trace'], description: 'shell print trace' },
+      { keys: ['-y', '--yes'], description: 'shell run skip prompt' },
+    ]
   }
 }
